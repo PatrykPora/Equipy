3850d42ff6357a904c9d9eb0b528463f68abe932
PatrykPora <patrykporadev@gmail.com>
2025-03-06 12:36:00 +0100

    assets model repo service controller

---
+++ src/main/java/pl/elpepe/equipy/asset/AssetMapper.java
@@ -0,0 +1,40 @@
+package pl.elpepe.equipy.asset;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AssetMapper {
+
+
+    private final CategoryRepository categoryRepository;
+
+    public AssetMapper(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    AssetDto toDto(Asset asset) {
+        AssetDto assetDto = new AssetDto();
+        assetDto.setId(asset.getId());
+        assetDto.setName(asset.getName());
+        assetDto.setDescription(asset.getDescription());
+        assetDto.setSerialNumber(asset.getSerialNumber());
+        if (asset.getCategory() != null) {
+            assetDto.setCategory(asset.getCategory().getName());
+        }
+        return assetDto;
+    }
+
+    Asset toEntity(AssetDto assetDto) {
+        Asset asset = new Asset();
+        asset.setId(assetDto.getId());
+        asset.setName(assetDto.getName());
+        asset.setDescription(assetDto.getDescription());
+        asset.setSerialNumber(assetDto.getSerialNumber());
+        Optional<Category> category = categoryRepository.findByName(assetDto.getCategory());
+        category.ifPresent(asset::setCategory);
+        return asset;
+    }
+}
---
+++ src/test/java/pl/elpepe/equipy/asset/AssetControllerTest.java
@@ -0,0 +1,30 @@
+package pl.elpepe.equipy.asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AssetControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void shouldGetAllAssets() throws Exception {
+        mockMvc.perform(get("/api/assets")).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.length()").value(10))
+                .andDo(print());
+    }
+}
